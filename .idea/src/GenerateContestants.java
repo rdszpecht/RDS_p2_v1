bce093c81c26dcb881e1e31a35efeb93eca2a4de
rdszpecht <rd.szpecht@gmail.com>
2018-05-05 20:02:43 +0200

    Init

---
+++ .idea/src/GenerateContestants.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Random;
+
+public class GenerateContestants {
+    public static void main(String[] args) {
+        try (PrintWriter pw = new PrintWriter("contestants.txt")) {
+            int nContestants = 1000000;
+            pw.println(nContestants);
+            for (int i = 0; i < nContestants; i++) {
+                pw.println("Contestant "+(i+1)+": "+new Random().nextInt(nContestants*10));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
